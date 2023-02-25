f43bf844d61289150a0feecb09da6aba994ad9b8
JaronBurtnett <admin@jaronburtnett.net>
2023-02-24 22:56:03 -0700

    Recommended Changes

--- war11.py
+++ war11.py
@@ -8,7 +8,14 @@ import random
 import csv
 import json
 
-
+# I can see what you're doing here, creating a deck of cards then pulling out random ones and alternating between the two players.
+# The idea is good though you've done it a very messy way.
+# First off, the 'dc' variable is pretty much entirely pointless. You're using it to keep track of how many cards are left in the deck, but you could just use the length of the deck list with len(deck).
+# The check for 'if dc == 0' is redundant since the for loop will run dc times anyways and then stop.
+# Another reason not to use 'dc' is that it makes your code fragile. Say you change the makeup of the deck with more or less cards and forget to update dc to match. Your code will break since it will expect a different number of cards than it actually gets.
+# Second,  you're using 'dealt' to keep track of which card is being dealt that round. While not bad a bit messy. Simpler method would be to simply use the pop() method to remove the card from the deck and assign it to the player.
+# Third, you're using 'dealDir' to keep track of which player is getting the card. This truthfully isn't the worst method but if you assume that the deck is always even then you can just give each player a random card from the deck and then remove it from the deck.
+# I've created 'deckDeal2' as an example of how I would implement this.
 def deckDeal(user, comp):
     deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,
     9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13,14,14,14,14]
@@ -29,6 +36,23 @@ def deckDeal(user, comp):
         if dealDir > 2:
             dealDir = 1     
         deck.remove(deck[dealt])
+
+def deckDeal2(user, comp):
+    deck = generateDeck(0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4)
+
+    # Does assume the number of cards in the deck is even.
+    while len(deck) > 0:
+        user.cards.append(deck.pop(randint(0, len(deck) - 1)))
+        comp.cards.append(deck.pop(randint(0, len(deck) - 1)))
+
+# I consider this to be a clean way of generating the deck you want. The 'numberOfCards' is a list of numbers, the position of the number in the list represents the card's value (The first position representing the number of '1's) while the number in that place represents the number of those cards that you want. I consider this to be more readable and easier to understand.
+# The 'enumerate' function is a nice way to get the index and value of each item in a list. It's a bit more efficient than using a for loop and keeping track of the index yourself.
+def generateDeck(*numberOfCards):
+    deck = []
+    for cardValue, numberOfCards in enumerate(numberOfCards):
+        for i in range(numberOfCards):
+            deck.append(cardValue + 1)
+    return deck
 
 def logDecks(y,c,t):
     aCounter = len(t.track) +1
@@ -167,25 +191,64 @@ deckDeal(user, comp) #distributing the entire deck list between the user and the
 #comp.cards = [5, 4, 3, 12, 8, 4, 2, 14, 14, 11, 9, 13, 14, 2, 8, 14, 12, 11, 7, 13, 10, 13, 12, 6, 6, 11, 6, 4, 3, 10, 9, 5, 5, 8, 7, 2, 3, 12, 7, 10, 3, 11, 9, 9, 7, 13, 10] #use to test scenarios
 
 while len(user.cards) != 0 and len(comp.cards) != 0:
-    if len(user.cards) == 0:
-        print('you lose')
-        break
-    elif len(comp.cards) == 0:
-        print('you win') 
-        break      
-    elif user.cards[0] > comp.cards[0]:
-        (user, comp, theGame) = youWin(user, comp, theGame) 
+    # Since the loop will be breaking when one of the decks is empty, I don't need to check for that here. I moved it to outside of the loop.
+    # if len(user.cards) == 0:
+    #     print('you lose')
+    #     break
+    # elif len(comp.cards) == 0:
+    #     print('you win') 
+    #     break   
+
+    playerCard = user.cards.pop()
+    compCard = comp.cards.pop()
+    pool = [playerCard, compCard]
+
+    while True:
+        print(str(playerCard) + ' ' + str(compCard))
+        if playerCard > compCard:
+            print('you win')
+            user.cards.extend(pool)
+            break
+        elif playerCard < compCard:
+            print('you lose')
+            comp.cards.extend(pool)
+            break
+        elif playerCard == compCard:
+            if len(user.cards) == 0:
+                print('you lose')
+                comp.cards.extend(pool)
+                break
+            elif len(comp.cards) == 0:
+                print('you win')
+                user.cards.extend(pool)
+                break
+            else:
+                for _ in range(3):
+                    if len(user.cards) > 1: # Ensures the last card in the deck is not added to the pool
+                        pool.append(user.cards.pop())
+                    if len(comp.cards) > 1:
+                        pool.append(comp.cards.pop())
+                playerCard = user.cards.pop()
+                compCard = comp.cards.pop()
         
-    elif user.cards[0] < comp.cards[0]:
-        (user, comp, theGame) = youLose(user, comp, theGame)
+# You're attempt here was pretty good. Honestly this was a challenge for me to figure out a cleaner way. I'm just going to put what I came up with and let you think about it.
+    # elif user.cards[0] > comp.cards[0]:
+    #     (user, comp, theGame) = youWin(user, comp, theGame) 
         
-    elif user.cards[0] == comp.cards[0]:
-        (user, comp, theGame) = warCardPlay(user, comp, theGame)
+    # elif user.cards[0] < comp.cards[0]:
+    #     (user, comp, theGame) = youLose(user, comp, theGame)
         
+    # elif user.cards[0] == comp.cards[0]:
+    #     (user, comp, theGame) = warCardPlay(user, comp, theGame)
+
+if len(user.cards) == 0:
+    print('you lose the game...')
+elif len(comp.cards) == 0:
+    print('you win the game!')
     
 #writeGame(theGame)
-logDecks(user, comp,theGame) #just needed tp show the final outcome of who has no cards and the other with the whole deck. 
-logGame(user.deckAtHand, comp.deckAtHand)
+#logDecks(user, comp,theGame) #just needed tp show the final outcome of who has no cards and the other with the whole deck. 
+#logGame(user.deckAtHand, comp.deckAtHand)
 #theSwap = chooseCard(theGame)
 #theGame = divideTracking(theGame)
 #theGame = repositionCards(theGame, theSwap)
